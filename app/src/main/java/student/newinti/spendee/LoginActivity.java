a3fbdb1b71f677187d8573bd0428e394d8c61c24
stephen <stephen@globalconnect.com.my>
2024-09-27 21:32:41 +0800

    Created the Login Interface

---
+++ app/src/main/java/student/newinti/spendee/LoginActivity.java
@@ -0,0 +1,51 @@
+package student.newinti.spendee;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity {
+    //  declare the id variables
+    private EditText email_input, password_input;
+    private Button login_button;
+    private Text login_success_toast, login_fail_toast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email_input = findViewById(R.id.email_input);
+        password_input = findViewById(R.id.password_input);
+        login_button = findViewById(R.id.login_button);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String email = email_input.getText().toString().trim();
+                String password = password_input.getText().toString().trim();
+
+//            For testing purpose where email and password not empty then able proceed
+                if (!email.isEmpty() && !password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, R.string.login_success_toast, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, R.string.login_fail_toast, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
+
+    }
+}
